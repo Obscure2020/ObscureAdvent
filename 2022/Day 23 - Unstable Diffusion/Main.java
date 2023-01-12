36466dc1d2c21a2fa85810d45938f6d2946492e7
Joey Sodergren <jas.elgoog@gmail.com>
2023-01-12 14:23:39 -0500

    Minor Cleanups

--- 2022/Day 23 - Unstable Diffusion/Main.java
+++ 2022/Day 23 - Unstable Diffusion/Main.java
@@ -19,23 +19,19 @@ class Main{
     IntPoint[] checks = new IntPoint[0];
     for(int zz=offset; zz<4+offset; zz++){
       int m = zz % 4;
-      if(m == 0){
-        //North
+      if(m == 0){ //North
         checks = IntStream.rangeClosed(-1,1).mapToObj(i->new IntPoint(e.x+i, e.y-1)).toList().toArray(checks);
         if(Arrays.stream(checks).noneMatch(elves::contains)) return new IntPoint(e.x, e.y-1);
       }
-      if(m == 1){
-        //South
+      if(m == 1){ //South
         checks = IntStream.rangeClosed(-1,1).mapToObj(i->new IntPoint(e.x+i, e.y+1)).toList().toArray(checks);
         if(Arrays.stream(checks).noneMatch(elves::contains)) return new IntPoint(e.x, e.y+1);
       }
-      if(m == 2){
-        //West
+      if(m == 2){ //West
         checks = IntStream.rangeClosed(-1,1).mapToObj(i->new IntPoint(e.x-1, e.y+i)).toList().toArray(checks);
         if(Arrays.stream(checks).noneMatch(elves::contains)) return new IntPoint(e.x-1, e.y);
       }
-      if(m == 3){
-        //East
+      if(m == 3){ //East
         checks = IntStream.rangeClosed(-1,1).mapToObj(i->new IntPoint(e.x+1, e.y+i)).toList().toArray(checks);
         if(Arrays.stream(checks).noneMatch(elves::contains)) return new IntPoint(e.x+1, e.y);
       }
@@ -53,31 +49,28 @@ class Main{
     scan.close();
     int offset=0, zz=1;
     for(; needsMoving(elves); zz++){
-      int[] needs = IntStream.range(0,elves.size()).filter(i->needsMoving(elves.get(i),elves)).toArray();
+      final int[] needs = IntStream.range(0,elves.size()).parallel().filter(i->needsMoving(elves.get(i),elves)).toArray();
       ArrayList<IntPoint> wants = new ArrayList<>(needs.length);
       final int offCopy = offset;
       Arrays.stream(needs).mapToObj(i->propose(elves.get(i),elves,offCopy)).forEachOrdered(wants::add);
-      int[] freqs = wants.stream().mapToInt(e->Collections.frequency(wants,e)).toArray();
-      for(int i=0; i<needs.length; i++){
-        if(freqs[i]>1) continue;
-        elves.set(needs[i], wants.get(i));
-      }
+      final int[] freqs = wants.stream().mapToInt(e->Collections.frequency(wants,e)).toArray();
+      for(int i=0; i<needs.length; i++) if(freqs[i] == 1) elves.set(needs[i], wants.get(i));
       offset = (offset + 1) % 4;
       if(zz == 10){
-        int part1 = 0;
         int minX = elves.stream().mapToInt(e->e.x).min().getAsInt();
         int minY = elves.stream().mapToInt(e->e.y).min().getAsInt();
         int maxX = elves.stream().mapToInt(e->e.x).max().getAsInt();
         int maxY = elves.stream().mapToInt(e->e.y).max().getAsInt();
+        int freeCount = 0;
         for(int y=minY; y<=maxY; y++){
           for(int x=minX; x<=maxX; x++){
             IntPoint n = new IntPoint(x,y);
-            if(!elves.contains(n)) part1++;
+            if(!elves.contains(n)) freeCount++;
           }
         }
-        System.out.println("Part #1 - " + part1);
+        System.out.println("Part #1 - " + freeCount);
       }
-      if(zz % 20 == 0) System.out.print(zz + "...\r");
+      if(zz % 20 == 0) System.out.print(" " + zz + "...\r");
     }
     System.out.println("Part #2 - " + zz);
   }
