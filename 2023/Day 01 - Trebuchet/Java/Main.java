1bb7b070eda8b5aa403ff30051d6900cb92add18
Joey Sodergren <jas.elgoog@gmail.com>
2023-12-01 15:51:34 -0500

    Improved Day 1

--- 2023/Day 01 - Trebuchet/Java/Main.java
+++ 2023/Day 01 - Trebuchet/Java/Main.java
@@ -1,59 +1,40 @@
 import java.util.*;
 import java.io.*;
 class Main{
-  private static String[] partTwoKeys = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-  private static int[] partTwoVals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  private static boolean isDigit(char input){
-    return input >= '0' && input <= '9';
-  }
-  private static int partOneValue(String input){
-    final int len = input.length();
-    int firstDigit = 0;
-    int lastDigit = 0;
-    for(int i=0; i<len; i++){
-      char c = input.charAt(i);
-      if(isDigit(c)){
-        firstDigit = Character.getNumericValue(c);
-        break;
-      }
-    }
-    for(int i=len-1; i>=0; i--){
-      char c = input.charAt(i);
-      if(isDigit(c)){
-        lastDigit = Character.getNumericValue(c);
-        break;
-      }
-    }
-    return firstDigit * 10 + lastDigit;
-  }
-  private static int partTwoValue(String input){
+  private final static String[] keysOne = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+  private final static String[] keysTwo = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; //The description of the problem does not list "zero" as a textual option.
+  private final static int[] values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  private static int calcValue(String input, String[] keys){
     int firstIndex = Integer.MAX_VALUE;
     int firstDigit = 0;
     int lastIndex = Integer.MIN_VALUE;
     int lastDigit = 0;
-    for(int i=0; i<partTwoKeys.length; i++){
-      final int leftIndex = input.indexOf(partTwoKeys[i]);
+    for(int i=0; i<keys.length; i++){
+      final int leftIndex = input.indexOf(keys[i]);
       if(leftIndex < 0) continue;
-      final int rightIndex = input.lastIndexOf(partTwoKeys[i]);
+      final int rightIndex = input.lastIndexOf(keys[i]);
       if(leftIndex < firstIndex){
         firstIndex = leftIndex;
-        firstDigit = partTwoVals[i];
+        firstDigit = values[i];
       }
       if(rightIndex > lastIndex){
         lastIndex = rightIndex;
-        lastDigit = partTwoVals[i];
+        lastDigit = values[i];
       }
     }
     return firstDigit * 10 + lastDigit;
   }
   public static void main(String[] args) throws Exception{
     Scanner scan = new Scanner(new File("input.txt"));
-    ArrayList<String> input = new ArrayList<>();
+    int partOne = 0;
+    int partTwo = 0;
     while(scan.hasNext()){
-      input.add(scan.nextLine());
+      String s = scan.nextLine();
+      partOne += calcValue(s, keysOne);
+      partTwo += calcValue(s, keysTwo);
     }
     scan.close();
-    System.out.println("Part #1 - " + input.stream().mapToInt(Main::partOneValue).sum());
-    System.out.println("Part #2 - " + input.stream().mapToInt(Main::partTwoValue).sum());
+    System.out.println("Part #1 - " + partOne);
+    System.out.println("Part #2 - " + partTwo);
   }
 }
