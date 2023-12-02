8f404c5cc1624d64f6cc7d80292557e6020e5767
Joey Sodergren <jas.elgoog@gmail.com>
2023-12-02 16:40:51 -0500

    Condensing syntax

--- 2023/Day 01 - Trebuchet/Java/Main.java
+++ 2023/Day 01 - Trebuchet/Java/Main.java
@@ -1,14 +1,8 @@
 import java.util.*;
 import java.io.*;
 class Main{
-  private final static String[] keysOne = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-  private final static String[] keysTwo = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; //The description of the problem does not list "zero" as a textual option.
-  private final static int[] values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  private static int calcValue(String input, String[] keys){
-    int firstIndex = Integer.MAX_VALUE;
-    int firstDigit = 0;
-    int lastIndex = Integer.MIN_VALUE;
-    int lastDigit = 0;
+  private static int calcValue(String input, String[] keys, int[] values){
+    int firstIndex = Integer.MAX_VALUE, firstDigit = 0, lastIndex = Integer.MIN_VALUE, lastDigit = 0;
     for(int i=0; i<keys.length; i++){
       final int leftIndex = input.indexOf(keys[i]);
       if(leftIndex < 0) continue;
@@ -25,13 +19,15 @@ class Main{
     return firstDigit * 10 + lastDigit;
   }
   public static void main(String[] args) throws Exception{
+    final String[] keysOne = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    final String[] keysTwo = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; //The description of the problem does not list "zero" as a textual option.
+    final  int[] values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     Scanner scan = new Scanner(new File("input.txt"));
-    int partOne = 0;
-    int partTwo = 0;
+    int partOne = 0, partTwo = 0;
     while(scan.hasNext()){
       String s = scan.nextLine();
-      partOne += calcValue(s, keysOne);
-      partTwo += calcValue(s, keysTwo);
+      partOne += calcValue(s, keysOne, values);
+      partTwo += calcValue(s, keysTwo, values);
     }
     scan.close();
     System.out.println("Part #1 - " + partOne);
